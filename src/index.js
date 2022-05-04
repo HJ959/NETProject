8082370b09ef042c9ec98ba2959251679d59b04e
HJ959 <henryjaames@gmail.com>
2022-05-04 22:02:59 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,46 @@
+// import js modules
+import * as PIXI from 'pixi.js';
+
+// throw in the css as well
+import './css/normalise.css'
+import './css/main.css'
+
+// variables
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+// The application will create a renderer using WebGL, if possible,
+// with a fallback to a canvas render. It will also setup the ticker
+// and the root stage PIXI.Container
+const app = new PIXI.Application({
+    resizeTo: window
+});
+
+// The application will create a canvas element for you that you
+// can then insert into the DOM
+document.body.appendChild(app.view);
+
+// load the texture we need
+app.loader.add('bunny', 'media/nicebathmatt.png').load((loader, resources) => {
+    // This creates a texture from a 'bunny.png' image
+    const bunny = new PIXI.Sprite(resources.bunny.texture);
+
+    // Setup the position of the bunny
+    bunny.x = app.renderer.width / 2.2;
+    bunny.y = app.renderer.height / 2.2;
+
+    // Rotate around the center
+    bunny.anchor.x = 0.5;
+    bunny.anchor.y = 0.5;
+
+    // Add the bunny to the scene we are building
+    app.stage.addChild(bunny);
+
+    // Listen for frame updates
+    app.ticker.add(() => {
+        // each frame we spin the bunny around a bit
+        bunny.rotation += 0.008;
+    });
+});
